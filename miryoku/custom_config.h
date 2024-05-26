e769fbc8f2f58ad5b92eff9cb99a8f50de92291d
Phil Donahue <pdonahue@brandgage.com>
2024-05-26 15:24:10 -0400

    moved ugui button to center index finger on both halves

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,6 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,                         &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,                         &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
+&kp A,     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    U_MT(LGUI, G),                         U_MT(LGUI, H),             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     &kp SEMI,   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,                         &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),            U_LT(U_SYM, BSPC),  U_LT(U_NUM, RET), U_LT(U_FUN, DEL),  U_NP,              U_NP

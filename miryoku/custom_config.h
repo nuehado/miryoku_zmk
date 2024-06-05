d5a762157e05be3d82183e3d85cf8285cff8a7a0
nuehado <51039077+nuehado@users.noreply.github.com>
2024-06-05 09:41:04 -0400

    moved mods to bottom row in symbol layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,3 +6,9 @@
 &kp A,     &kp S,     &kp D,    &kp F,    &kp G,                                             &kp H, &kp J, &kp K, &kp L, &kp SEMI,\
 U_LT(U_BUTTON, Z), U_MT(LALT, X), U_MT(LCTRL, C), U_MT(LSHFT, V), U_MT(LGUI, B),             U_MT(LGUI, N),  U_MT(LSHFT, M), U_MT(LCTRL, COMMA), U_MT(LALT, DOT), U_LT(U_BUTTON, SLASH),\
 U_NP, U_NP, U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),                        U_LT(U_SYM, BSPC),  U_LT(U_NUM, RET), U_LT(U_FUN, DEL),  U_NP, U_NP
+
+#define MIRYOKU_ALTERNATIVES_SYM \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              U_NA,              U_NA,              U_NA,              U_NA,          \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          &kp LGUI,          &kp LSHFT,         &kp LCTRL,         &kp LALT,          U_NA,              \
+U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP

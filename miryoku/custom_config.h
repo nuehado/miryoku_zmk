63efdf614ad8700a4a432eb9ff1ba38040324263
nuehado <51039077+nuehado@users.noreply.github.com>
2024-06-19 14:42:38 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,12 +10,12 @@ U_NP, U_NP, U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),           
 
 #define MIRYOKU_LAYER_NAV \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp INS,           &kp HOME,          &kp UP,            &kp END,           &kp PG_UP,         \
-U_NA,              &kp RALT,          U_NA,              U_NA,              U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp PG_DN,         \
-U_NA,              &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp LGUI,          U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp PG_DN,         \
+&kp LALT,          &kp RALT,          &kp LCTRL,         &kp LSHFT,         &kp LGUI,          U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 &kp SINGLE_QUOTE,      &kp DOUBLE_QUOTES,     &kp CARET,        &kp BACKSLASH,    &kp GRAVE,        &kp LEFT_BRACKET,       &kp LESS_THAN,            &kp EQUAL,     &kp GREATER_THAN,         &kp RIGHT_BRACKET, \
 &kp EXCLAMATION,       &kp AT_SIGN,           &kp HASH,         &kp DOLLAR,       &kp AMPERSAND,    &kp LEFT_BRACE,         &kp LEFT_PARENTHESIS,     &kp PIPE,      &kp RIGHT_PARENTHESIS,    &kp RIGHT_BRACE,   \
 &kp PERCENT,           &kp SLASH,             &kp MINUS,        &kp PLUS,         &kp ASTERISK,     &kp UNDERSCORE,         U_NA,                     U_NA,          U_NA,                     U_NA,              \
-U_NP,                  U_NP,                 U_NA,              U_NA,             U_NA,             U_NA,                   U_NA,                     U_NA,          U_NP,                     U_NP
+U_NP,                  U_NP,                 U_NA,              U_NA,             U_NA,             U_NA,                   U_NA,                     U_NA,          U_NP,                     U_NPdfdf

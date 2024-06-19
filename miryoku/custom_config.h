59837e3e5c73edca0b65a3a1e935064b45499b98
nuehado <51039077+nuehado@users.noreply.github.com>
2024-06-19 10:40:58 -0400

    Update custom_config.h
    i think the layer names were wrong

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,13 +7,13 @@
 U_LT(U_BUTTON, Z), U_MT(LALT, X), U_MT(LCTRL, C), U_MT(LSHFT, V), U_MT(LGUI, B),             U_MT(LGUI, N),  U_MT(LSHFT, M), U_MT(LCTRL, COMMA), U_MT(LALT, DOT), U_LT(U_BUTTON, SLASH),\
 U_NP, U_NP, U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_MOUSE, SPACE),                        U_LT(U_SYM, BSPC),  U_LT(U_NUM, RET), U_LT(U_FUN, DEL),  U_NP, U_NP
 
-#define MIRYOKU_ALTERNATIVES_SYM \
+#define MIRYOKU_LAYER_SYM \
 &kp APOS,          &kp DQT,          &kp CARET,        &kp BSLH,         &kp GRAVE,        &kp LBKT,          &kp LT,               &kp EQUAL,     &kp RT,       &kp RBKT,            \
 &kp EXCL,          &kp AT,           &kp NUHS,         &kp DLLR,         &kp AMP,          &kp LBRC,          &kp LPAR,             &kp PIPE,      &kp RPAR,     &kp RBRC,          \
 &kp PRCNT,         &kp FSLH,         &kp MINUS,        &kp PLUS,         &kp STAR,         &kp UNDER,         &kp EQUAL &kp RT,     U_NA,          U_NA,         U_NA,              \
 U_NP,              U_NP,             &kp LPAR,         &kp RPAR,         &kp UNDER,        U_NA,              U_NA,                 U_NA,          U_NP,         U_NP
 
-#define MIRYOKU_ALTERNATIVES_NAV \
+#define MIRYOKU_LAYER_NAV \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp INS,           &kp HOME,          &kp UP,            &kp END,           &kp PG_UP,         \
 U_NA,              &kp RALT,          U_NA,              U_NA,              U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp PG_DN,         \
 U_NA,              &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp LGUI,          U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \

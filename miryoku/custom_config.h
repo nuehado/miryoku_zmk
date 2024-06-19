4926393efc156dc173629ed9e6d264303a2f7e8d
nuehado <51039077+nuehado@users.noreply.github.com>
2024-06-19 14:44:32 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_CLIPBOARD_WIN \
+
 #define MIRYOKU_LAYER_BASE \
 &kp Q,     &kp W,     &kp E,    &kp R,    &kp T,                                             &kp Y,  &kp U, &kp I, &kp O, &kp P,\
 &kp A,     &kp S,     &kp D,    &kp F,    &kp G,                                             &kp H, &kp J, &kp K, &kp L, &kp SEMI,\
